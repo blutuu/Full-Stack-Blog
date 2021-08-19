4f68e599486e3b73448a65891742018d654a2b47
blutuu <blutuu1@gmail.com>
2021-08-19 16:35:50 -0400

    Minor server updates

--- blog-backend/src/server.js
+++ blog-backend/src/server.js
@@ -2,10 +2,31 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
+const articlesInfo = {
+  'learn-react': {
+    upvotes: 0
+  },
+  'learn-node': {
+    upvotes: 0
+  },
+  'my-thoughts-on-resumes': {
+    upvotes: 0
+  }
+};
 
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.get('/hello', (req, res) => res.send('Hello!'));
-app.post('/hello', (req, res) => res.send(`Hello! ${req.body.name}!`));
+app.post('/api/articles/:name/upvote', (req, res) => {
+  const articleName = req.params.name;
+
+  articlesInfo[articleName].upvotes += 1;
+  res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes.`);
+});
+
+app.post('/api/articles/:name/view-upvotes', (req, res) => {
+  const articleName = req.params.name;
+
+  res.status(200).send(`${articleName} has ${articlesInfo[articleName].upvotes} upvotes.`);
+});
 
 app.listen(8000, () => console.log('Listening on port 8000'));
