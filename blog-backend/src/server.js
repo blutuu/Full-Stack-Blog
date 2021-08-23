3ecb59041b9c50bcef3cd116cd582ee8456aaea7
blutuu <blutuu1@gmail.com>
2021-08-23 17:24:57 -0400

    Server issues I'm working on

--- blog-backend/src/server.js
+++ blog-backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
+const port = process.env.PORT || 8000;
 const articlesInfo = {
   'learn-react': {
     upvotes: 0
@@ -19,8 +20,8 @@ app.use(express.json());
 app.post('/api/articles/:name/upvote', (req, res) => {
   const articleName = req.params.name;
 
-  articlesInfo[articleName].upvotes += 1;
-  res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes.`);
+  articlesInfo[articleName].upvotes += 1; 
+  res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`);
 });
 
 app.post('/api/articles/:name/view-upvotes', (req, res) => {
@@ -29,4 +30,7 @@ app.post('/api/articles/:name/view-upvotes', (req, res) => {
   res.status(200).send(`${articleName} has ${articlesInfo[articleName].upvotes} upvotes.`);
 });
 
-app.listen(8000, () => console.log('Listening on port 8000'));
+app.listen(port, () => console.log('Listening on port 8000')); 
+ 
+// process.on('SIGINT', () => { console.log("exiting…"); process.exit(); });
+// process.on('exit', () => { console.log("exiting…"); process.exit(); });
