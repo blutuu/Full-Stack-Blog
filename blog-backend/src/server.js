7cacf070f6aea31a3137d93df69411313b6ecd7c
blutuu <blutuu1@gmail.com>
2021-09-13 17:31:26 -0400

    Updated server and package.
    Fixed nodemon script issues.
    Added comment functionality to the server.

--- blog-backend/src/server.js
+++ blog-backend/src/server.js
@@ -5,13 +5,16 @@ const app = express();
 const port = process.env.PORT || 8000;
 const articlesInfo = {
   'learn-react': {
-    upvotes: 0
+    upvotes: 0,
+    comments: []
   },
   'learn-node': {
-    upvotes: 0
+    upvotes: 0,
+    comments: []
   },
   'my-thoughts-on-resumes': {
-    upvotes: 0
+    upvotes: 0,
+    comments: []
   }
 };
 
@@ -21,13 +24,23 @@ app.post('/api/articles/:name/upvote', (req, res) => {
   const articleName = req.params.name;
 
   articlesInfo[articleName].upvotes += 1; 
-  res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`);
+  res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`);
 });
 
 app.post('/api/articles/:name/view-upvotes', (req, res) => {
   const articleName = req.params.name;
 
   res.status(200).send(`${articleName} has ${articlesInfo[articleName].upvotes} upvotes.`);
+});
+
+app.post('/api/articles/:name/add-comment', (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+
+  articlesInfo[articleName].comments.push({username, text});
+
+  res.status(200).send(articlesInfo[articleName]);
+
 });
 
 app.listen(port, () => console.log('Listening on port 8000')); 
